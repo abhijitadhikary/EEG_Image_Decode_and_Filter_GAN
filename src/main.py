26de0a1c7171d09b58c7df475053f1cf09270c82
abhijitadhikary <abhi.drmc@gmail.com>
2021-05-11 12:11:58 +1000

    tensorboard fixed

--- src/main.py
+++ src/main.py
@@ -11,16 +11,24 @@ for index_epoch in range(args.start_epoch, args.num_epochs):
     args.loss_D_train_running.append(loss_D_epoch_train)
     args.loss_G_train_running.append(loss_G_epoch_train)
     print_loss(loss_D_epoch_train, loss_G_epoch_train, index_epoch, args.num_epochs, mode='train')
-    args.writer.add_scalar('Loss/train_D', loss_D_epoch_train, index_epoch+1)
-    args.writer.add_scalar('Loss/train_G', loss_G_epoch_train, index_epoch+1)
+    # args.writer.add_scalar('Loss/train_D', loss_D_epoch_train, index_epoch+1)
+    # args.writer.add_scalar('Loss/train_G', loss_G_epoch_train, index_epoch+1)
 
     # validate
     loss_D_epoch_val, loss_G_epoch_val = forward_pass(args, args.dataloader_val, mode='val')
     args.loss_D_val_running.append(loss_D_epoch_val)
     args.loss_G_val_running.append(loss_G_epoch_val)
     print_loss(loss_D_epoch_val, loss_G_epoch_val, index_epoch, args.num_epochs, mode='val')
-    args.writer.add_scalar('Loss/val_D', loss_D_epoch_val, index_epoch + 1)
-    args.writer.add_scalar('Loss/val_G', loss_G_epoch_val, index_epoch + 1)
+
+    args.writer.add_scalars('Loss', {
+        'train_D': loss_D_epoch_train,
+        'val_D': loss_D_epoch_val,
+        'train_G': loss_G_epoch_train,
+        'val_G': loss_G_epoch_val
+    }, index_epoch+1)
+    # args.writer.add_scalars('Loss/val', {'D': loss_D_epoch_val, 'G': loss_G_epoch_val}, index_epoch + 1)
+    # args.writer.add_scalar('Loss/val_D', loss_D_epoch_val, index_epoch + 1)
+    # args.writer.add_scalar('Loss/val_G', loss_G_epoch_val, index_epoch + 1)
 
     save_model(args, loss_D_epoch_val, loss_G_epoch_val)
     # plot_train_vs_val_loss(args.loss_D_train_running, args.loss_D_val_running, mode='D')
